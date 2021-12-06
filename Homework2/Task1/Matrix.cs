463cd9cbb4a3ac7317de63670a22e55e54bf3fe7
Lkz03 <andriuspr21@gmail.com>
2021-12-06 21:32:59 +0200

    Removed ToString() method from Matrix class, method ToString() is a system method

--- Homework2/Task1/Matrix.cs
+++ Homework2/Task1/Matrix.cs
@@ -1,5 +1,4 @@
 ﻿internal abstract class Matrix // should a modifier be used here ? Should it be internal ?
 {
- public abstract bool Equals(); // Can this be bool ? 
- public abstract string ToString();
+ public abstract bool Equals(); // Can this be bool ?
 }
