02e96b06aec2b3bb7ad2e8d4af6ce71f41b8eb65
Lkz03 <andriuspr21@gmail.com>
2021-12-07 18:59:10 +0200

    Added all required classes of Task2

--- Homework2/Task2/Program.cs
+++ Homework2/Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Coherent is developing a training management system. Each training consists of a set of
+lectures and practical exercises. It is necessary to create classes to represent the following
+entities: training, lecture, practical lesson.
+
+All specified entities have a text description (this is an arbitrary string, possibly empty or equal
+to null).
+
+A lecture has a topic (an arbitrary string, possibly empty or equal to null), and a practical lesson
+- a link to a task condition (an arbitrary string, possibly empty or equal to null) and a link to a
+solution (an arbitrary string, possibly empty or equal to null).
+The training should store a set of lectures and practice sessions in an internal array and have an
+Add() method for adding a lecture or practice session.In addition, the training must have an
+IsPractical() method - it returns true if the training contains only practical lessons.
+
+Implement the Clone() instance method in the training class to clone the training.
+*/
+
+class Program
+{
+ public static void Main()
+ {
+  
+ }
+}
