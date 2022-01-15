9c9a64b5b66a590463bad3ad47ff0f59195f43da
Lkz03 <andriuspr21@gmail.com>
2022-01-15 23:04:46 +0200

    Task2 -added IsDuplicate method -added IsKeyExists method -added GetBookByISBN method -added Add method -added an indexer to get books by ISBN -added tests in program.cs

--- Homework5/Task2/Collections/Catalog.cs
+++ Homework5/Task2/Collections/Catalog.cs
@@ -5,11 +5,66 @@ namespace Task2.Collections
 {
  class Catalog : IEnumerable<(string, Book)>
  {
-  private List<(string, Book)> _books;
+  private List<(string, Book)> _books = new List<(string, Book)> ();
 
-  public (string, Book) GetBookByISBN(string ISBN)
+  // doesnt check for a different ISBN format yet
+  private bool IsDuplicate((string ISBN, Book book) bookTuple)
   {
-   throw new NotImplementedException();
+   foreach (var book in _books)
+   {
+    if (book.Item1 == bookTuple.ISBN && book.Item2.Title == bookTuple.book.Title) // for simplisity I check only for titles instead of implementing Equals method
+    {
+     return true;
+    }
+   }
+   return false;
+  }
+
+  // doesnt check for a different ISBN format yet
+  private bool IsKeyExists(string ISBN)
+  {
+   foreach (var book in _books)
+   {
+    if (book.Item1 == ISBN)
+    {
+     return true;
+    }
+   }
+   return false;
+  }
+
+  // doesnt check for a different ISBN format yet
+  private Book GetBookByISBN(string ISBN)
+  {
+   foreach (var book in _books)
+   {
+    if (book.Item1 == ISBN)
+    {
+     return book.Item2 as Book;
+    }
+   }
+   throw new ArgumentException();
+  }
+  
+  public void Add(string ISBN, Book book)
+  {
+   if (IsDuplicate((ISBN, book)))
+   {
+    throw new ArgumentException();
+   }
+   _books.Add((ISBN, book));
+  }
+
+  public Book this[string ISBN]
+  {
+   get
+   {
+    if (!IsKeyExists(ISBN))
+    {
+     throw new KeyNotFoundException();
+    }
+    return GetBookByISBN(ISBN);
+   }
   }
 
   public IEnumerator<(string, Book)> GetEnumerator()
--- Homework5/Task2/Program.cs
+++ Homework5/Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task2.Collections;
+using Task2.Classes;
+
+public class Program
+{
+ public static void Main()
+ {
+  Book book1 = new Book("one");
+  Book book2 = new Book("two");
+  Book book3 = new Book("three");
+  //Book book4 = new Book("one"); //duplicate
+
+  Catalog catalog = new Catalog();
+
+  catalog.Add("1234567891231", book1);
+  catalog.Add("1234567891232", book2);
+  catalog.Add("1234567891233", book3);
+  //catalog.Add("1234567891231", book4); //ArgumentException, trying to add a duplicate
+
+  foreach (var item in catalog)
+  {
+   Console.WriteLine(item.Item1 + " " + item.Item2.Title);
+  }
+
+  Console.WriteLine($"\nBook by ISBN number: 1234567891232 - {catalog["1234567891232"].Title}");
+ }
+}
