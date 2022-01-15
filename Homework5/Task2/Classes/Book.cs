7a3bc5883d1b6d453ba31f41499717a7a2d6cbf9
Lkz03 <andriuspr21@gmail.com>
2022-01-15 22:05:04 +0200

    Task2 intial commit

---
+++ Homework5/Task2/Classes/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2.Classes
+{
+ class Book
+ {
+  private string _title;  
+  public List<string> Authors { get; set; }
+  public string Title 
+  {
+   get => _title;
+
+   private set 
+   {
+    if (String.IsNullOrEmpty(value))
+    {
+     throw new ArgumentNullException(nameof(value));
+    }
+    _title = value;
+   }
+  }
+  public DateOnly Date { get; set; }
+
+  public Book(string title) => Title = title;
+  public Book(string title, DateOnly date) : this(title) => Date = date;
+  public Book(string title, DateOnly date, List<string> authors) : this(title, date) => Authors = authors;
+ }
+}
---
+++ Homework5/Task2/Collections/Catalog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Task2.Classes;
+
+namespace Task2.Collections
+{
+ class Catalog : IEnumerable<(string, Book)>
+ {
+  private List<(string, Book)> _books;
+
+  public (string, Book) GetBookByISBN(string ISBN)
+  {
+   throw new NotImplementedException();
+  }
+
+  public IEnumerator<(string, Book)> GetEnumerator()
+  {
+   return _books.GetEnumerator();
+  }
+
+  IEnumerator IEnumerable.GetEnumerator()
+  {
+   return GetEnumerator();
+  }
+ }
+}
